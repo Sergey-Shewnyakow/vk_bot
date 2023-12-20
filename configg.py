1ccc0c1eb9f561da2ca5cc3e79cb35b734079acf
Sergey Shewnyakow <Sergey.Shewnyakow@yandex.ru>
2023-12-20 08:56:51 +0300

    transfer API to .env

--- configg.py
+++ configg.py
@@ -1,2 +1,6 @@
-tok = "vk1.a.u-J0EqveWwTtw-aQVi6NHYLcGHGjEiwL3GOpn-j-RRKlJ_orzGLFeX5DDXnHGocNPryz9rcRXUpOQq-yLC7YE6I6eV2fDDwA9-scx3GEthXwUVeqIPyAL7iRciY8Hdrvk8OH3fAwNTxoWNDI243GNA_yl9mkijroz9a7ZDlAJAaWWBL0Ys1AAplKRti-ZETUl5V1nGOT-pG5uZ60g9ek_w"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+tok = os.getenv('TOKEN')
 admin_id = 238119562
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from bot import VkBot
+from dotenv import load_dotenv
 
 if __name__ == '__main__':
+    load_dotenv()
     VkBot().run()
